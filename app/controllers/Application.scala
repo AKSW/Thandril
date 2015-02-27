3da810d87c6a954096ebc540b880df4e3a1d87b6
Roy Meissner <roymeissn@googlemail.com>
2015-02-27 17:53:15 +0100

    Added File-Upload, Added program whitelist
    Added simple Fileupload. not functional by now

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,14 +9,31 @@ import play.api.libs.json.Json.toJsFieldJsValueWrapper
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Thandril", getInstalledPrograms))
+  def index = Action { request =>
+
+    getInstalledPrograms match {
+      case Some(s) => Ok(views.html.index("Thandril", s))
+      case _ => InternalServerError
+    }
+
   }
-  
+
   def programInfo(program: String) = Action {
     val man = getManpage(program)
     val response = Json.obj("name" -> JsString(program), "description" -> JsString(man))
     Ok(response)
+  }
+
+  def upload = Action(parse.temporaryFile) { request =>
+    import java.io.File
+    
+    val directory = new File("./uploadedPrograms");
+    
+    if (!(directory exists))
+      directory.mkdir()
+      println(request.body)
+    request.body.moveTo(new File("./uploadedPrograms/xyz"), true)
+    Ok("File uploaded")
   }
 
 }
--- app/system/programs.scala
+++ app/system/programs.scala
@@ -1,25 +1,67 @@
 package system
 
-import scala.sys.process._
+import scala.sys.process.stringToProcess
+
+import play.api.Logger
 
 object programs {
 
+  val whitelist = Set("cat", "grep", "dmesg", "sed")
+  val dirs = List("/usr/bin", "./bin")
+
   /**
+   * Selects all installed programs of the host machine, filtered by whitelist
    * @todo find a way to print $path variabel
    */
   def getInstalledPrograms = {
-    val dirContents = "ls /usr/bin".!! split ('\n')
-    Map("System Programs" -> dirContents.tail)
+
+    try {
+      val dirContents = "ls /usr/bin".!! split ('\n')
+      Some(Map("System Programs" -> dirContents.tail.filter { x => whitelist.contains(x) }))
+    } catch {
+      case t: Throwable => Logger.info("Directory not found", t); None
+    }
+
   }
 
-  def saveProgram = ???
+  def saveProgram = {
+    if (!("ls").!!.contains("bin"))
+      "mkdir bin".!!
 
+    /*import java.io._
+
+    val directory = new File("./bin");
+    
+    if (!(directory exists)) {
+      if (directory mkdir) {
+        //Successfully created new directory
+      } else {
+        //Failed to create new directory
+      }
+    }*/
+  }
+
+  /**
+   * Returns the help text of the passed program
+   */
   def getParameters(program: String) = {
-    (program + " --help").!!
+    try {
+      (program + " --help").!!
+    } catch {
+      case t: Throwable => "There is no help text, I'm sorry"
+    }
   }
-  
+
+  /**
+   * Returns the manpage text of the passed program
+   */
   def getManpage(program: String) = {
-    ("man "+program).!!
+    try {
+      ("man " + program).!!
+    } catch {
+      case t: Throwable => "There is no manpage, I'm sorry"
+    }
+
   }
 
 }
